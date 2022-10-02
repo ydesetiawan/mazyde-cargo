5cdd86d953b9383e237edae7749adcef80774abf
Edy Setiawan <ydesetiawan@gmail.com>
2022-10-02 14:17:55 +0700

    Add log

--- src/test/java/GeneratePassword.java
+++ src/test/java/GeneratePassword.java
@@ -3,7 +3,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class GeneratePassword {
 
     public static void main(String[] args) {
-        String mekari123 = new BCryptPasswordEncoder().encode("mekari123");
+        String mekari123 = new BCryptPasswordEncoder().encode("admin123");
         System.out.println(mekari123);
     }
 
